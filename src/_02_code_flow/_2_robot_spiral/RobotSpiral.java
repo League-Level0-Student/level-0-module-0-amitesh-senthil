6c4325a0af60b1b87d9be90d0cae470a1f66c84f
student <student@ip-172-31-53-13.us-west-2.compute.internal>
2021-05-19 17:00:56 -0700

    i did pentagon crazy robot spiral

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -12,31 +12,32 @@ public class RobotSpiral {
 	public static void main(String[] args) {
 		
 		// Create a new Robot
-		
+		Robot spiral_bobb = new Robot();
 		// Set your robot's pen down 
-		
+		spiral_bobb.penDown();
 		// SPEED. Set the robot to go at max speed (100)
-
+		spiral_bobb.setSpeed(10);
 		// COUNT. Create an int variable that will count how many lines of the spiral we have drawn.
-			//        Start its value as zero.
-
+		//        Start its value as zero.
+		int count = 0;
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
-
+		while ( count < 50 ) {
 			// COLOR.   Have the robot set a random pen color:      setRandomPenColor()
-
+			spiral_bobb.setRandomPenColor();
 			// DRAW.    Move the robot (5*count) pixels
+			spiral_bobb.move(5*count);
 			//          count is the name of the variable you created earlier
-
+			count +=1;
 			// TURN.    Turn the robot (360/7) degrees to the right
-
+			spiral_bobb.turn(360/7);
 			// COUNT.   Increase the count of how many lines have been drawn so far ( count+=1 )
-
+			count +=1;
 			// Change the robot pen width to the current value of the count variable 
-
+			spiral_bobb.setPenWidth(count);
 
 		//  End the while loop here
 
-		
+		}
 		// Run the program. 
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
 		
