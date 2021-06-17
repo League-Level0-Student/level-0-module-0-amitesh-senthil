e52cf6783a693ead4d089d3cce2ac1bde0ded5b8
senumamit <senumamit@192.168.1.13>
2021-06-16 17:00:42 -0700

    I did world domination

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -16,7 +16,7 @@ public class WorldDomination {
 		// 3. Otherwise, tell them to sign up for classes at The League.
 		else {
 			JOptionPane.showInputDialog("sign up for classes at the league");
-			// https://www.wattpad.com/1047111523-living-together-dnf-old-memories
+			// https://www.wattpad.com/1047325532-living-together-dnf-deep-breaths
 		}
 			
 
